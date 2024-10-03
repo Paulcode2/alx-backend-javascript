1b8427d80c984d6586ffc7b31b210d2c850a8966
paul <paullevites84@gmail.com>
2024-10-03 11:40:36 +0100

    fixes

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -5,7 +5,7 @@ export default function createReportObject(employeesList) {
         ...employeesList,
       },
       getNumberOfDepartments(employeesList) {
-        return Object.keys(employeesList).length;
+        return Object.keys(employeesList).length
       },
     };
   }
