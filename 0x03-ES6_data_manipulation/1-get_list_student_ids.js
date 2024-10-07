257e7ae34dbcf17ae9b0cb757dbb52d8314c8667
paul <paullevites84@gmail.com>
2024-10-08 00:09:46 +0100

    Task 1-10

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+
+const getListStudentIds = (listStudents) => {
+    let listStudentIds = [];
+    if (!(listStudents instanceof Array)) {
+      return listStudentIds;
+    }
+    listStudentIds = listStudents.map((student) => student.id);
+    return listStudentIds;
+  };
+  
+  export default getListStudentIds;
+  
